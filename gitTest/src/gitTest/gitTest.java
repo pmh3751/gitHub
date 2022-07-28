12b53d0273735a27c2f57e70e9e4e528084915e6
tj-bu <tj-bu@DESKTOP-D97NQAG>
2022-07-28 16:58:29 +0900

    q112

--- gitTest/src/gitTest/gitTest.java
+++ gitTest/src/gitTest/gitTest.java
@@ -10,7 +10,7 @@ public class gitTest {
 			for(int j = 1; j < 10; j++) {
 				System.out.println(i + " x " + j + " = " + (i*j));
 			}
-			System.out.println();
+			System.out.println("qqqqqqqqq");
 		}
 		
 		

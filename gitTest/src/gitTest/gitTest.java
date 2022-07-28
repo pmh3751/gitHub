c43c893aaf122d9f005b12a9fd4c0696e7533f6c
tj-bu <tj-bu@DESKTOP-D97NQAG>
2022-07-28 16:43:50 +0900

    첫번째 깃 실습 = 구구단 짜기

---
+++ gitTest/src/gitTest/gitTest.java
@@ -0,0 +1,66 @@
+package gitTest;
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 2; i < 10; i++){
+			System.out.println(i + "´Ü");
+			for(int j = 1; j < 10; j++) {
+				System.out.println(i + " x " + j + " = " + (i*j));
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
